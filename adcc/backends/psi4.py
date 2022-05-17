b5c7fac35acb2d9fe92f0bef1d5b858d32ed6f56
BauerMarco <marcba1@web.de>
2022-05-17 16:01:37 +0200

    introduced different orbital energy loader, which is required for psi4numpy qed-rhf manipulation and loading

--- adcc/backends/psi4.py
+++ adcc/backends/psi4.py
@@ -217,8 +217,12 @@ class Psi4HFProvider(HartreeFockProvider):
         )
 
     def fill_orben_f(self, out):
-        orben_a = np.asarray(self.wfn.epsilon_a())
-        orben_b = np.asarray(self.wfn.epsilon_b())
+        #orben_a = np.asarray(self.wfn.epsilon_a())
+        #orben_b = np.asarray(self.wfn.epsilon_b())
+        orben_a = np.asarray(self.wfn.Fa()).diagonal()
+        orben_b = np.asarray(self.wfn.Fb()).diagonal()
+        #print("orben_a ", orben_a)
+        #print("orben_b", orben_b)
         out[:] = np.hstack((orben_a, orben_b))
 
     def fill_occupation_f(self, out):
